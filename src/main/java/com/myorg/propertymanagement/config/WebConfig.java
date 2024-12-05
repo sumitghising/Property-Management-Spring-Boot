b262c133ce0acf53b46cd85412ada1d0e2cd03c5
suresh_11 <sureshsubedi485@gmail.com>
2024-12-05 16:05:08 +0545

    Setup interceptor

---
+++ src/main/java/com/myorg/propertymanagement/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.myorg.propertymanagement.config;
+
+import com.myorg.propertymanagement.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor()).addPathPatterns("/**");
+    }
+}
---
+++ src/main/java/com/myorg/propertymanagement/interceptor/LogInterceptor.java
@@ -0,0 +1,33 @@
+package com.myorg.propertymanagement.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class LogInterceptor implements HandlerInterceptor {
+
+    private Logger logger = LoggerFactory.getLogger(LogInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        logger.info("preHandle");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
+        logger.info("postHandle");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+            throws Exception {
+        logger.info("afterCompletion");
+    }
+
+}
